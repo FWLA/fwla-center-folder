97e58be469be26f9275bf02bfa800d3b7917a3b8
Benjamin Ihrig <benjamin.ihrig@sap.com>
2019-02-01 12:27:47 +0100

    Move back to pipenv

--- fwla-center-folder.py
+++ fwla-center-folder.py
@@ -1,6 +1,19 @@
-class LedControler:
+class LedController:
     def reset(self):
         pass
 
     def set(self, id):
         pass
+
+
+class PrintoutLedController(LedController):
+    def reset(self):
+        print('Reset')
+
+    def set(self, id):
+        print('set {}'.format(id))
+
+if __name__ == "__main__":
+    led_controller = PrintoutLedController()
+    led_controller.reset()
+    led_controller.set(1)
